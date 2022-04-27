448fe1a784dae64f9e8e35e2abf491e11d17d4a7
pb-bennett <pb.bennett.uk@gmail.com>
2022-04-27 13:57:11 +0200

    Working on functions - still bad at remembering to commit :(

--- script/script.js
+++ script/script.js
@@ -5,17 +5,28 @@ const mainDisplay = document.getElementById("main-display");
 const upperDisplay = document.getElementById("upper-display");
 const btnNumberAll = document.querySelectorAll("btn-num");
 const btnAll = document.querySelectorAll(".btn");
+const debugContainer = document.getElementById("debug-output");
+const debugClearBtn = document.getElementById("debug-clear");
+const debugTestBtn = document.getElementById("debug-test");
 
-let firstNumbers, secondNubers, operators, currentString, upperString, counter;
+let firstNumbersArr,
+  secondNubersArr,
+  operatorsArr,
+  evaluationsArr,
+  currentString,
+  upperString,
+  counter;
 
 const init = function () {
-  firstNumbers = [];
-  secondNubers = [];
-  operators = [];
+  firstNumbersArr = [];
+  secondNubersArr = [];
+  operatorsArr = [];
+  evaluationsArr = [];
   currentString = "0";
   upperString = "";
   counter = 0;
   updateScreen();
+  testFunction("init");
 };
 
 const updateScreen = function () {
@@ -29,12 +40,15 @@ const btnClick = function (input, type) {
   if (type === "action") actionClick(input);
   if (type === "operator") operatorClick(input);
   updateScreen();
+  // testFunction("buttonClick", "purple");
 };
 
 const numberClick = function (input) {
   if (currentString === "0") currentString = "";
+  // if(operatorsArr[counter])
   currentString = currentString + input;
   console.log(currentString);
+  testFunction("numberClick", "green");
   // updateScreen();
 };
 const actionClick = function (input) {
@@ -42,24 +56,66 @@ const actionClick = function (input) {
   if (input === "point") point();
   if (input === "clear") init();
   if (input === "+/-") signChange();
+  if (input === "evaluate") evaluate();
   console.log(currentString);
+  testFunction("actionClick", "orangered");
   // updateScreen();
 };
-const operatorClick = function (input) {};
+
+const operatorClick = function (input) {
+  if (
+    firstNumbersArr[counter] &&
+    secondNubersArr[counter] &&
+    operatorsArr[counter]
+  )
+    evaluate();
+  if (!currentString || currentString === "0") return;
+  if (!firstNumbersArr[counter])
+    firstNumbersArr[counter] = Number(currentString);
+  if (firstNumbersArr[counter])
+    secondNubersArr[counter] = Number(currentString);
+  operatorsArr[counter] = input;
+  upperString = `${firstNumbersArr[counter]} ${operatorsArr[counter]}`;
+
+  currentString = "";
+  testFunction("operatorClick", "orangered");
+};
+const evaluate = function () {
+  console.log(
+    `evaluating: ${firstNumbersArr[counter]} ${operatorsArr[counter]} ${secondNubersArr[counter]}`
+  );
+  testFunction("evaluate", "red");
+};
 
 const backspace = function () {
   currentString = currentString.slice(0, -1);
   if (currentString === "") currentString = "0";
+  testFunction("backspace", "blue");
 };
 
 const point = function () {
   if (currentString.includes(".")) return;
   currentString += ".";
-  // updateScreen();
+  testFunction("point", "blue");
 };
 
 const signChange = function () {
   currentString = (currentString * -1).toString();
+  testFunction("signChange", "blue");
+};
+
+const testFunction = function (inputFunction, color = "black") {
+  const text = `${counter}:  Input function: <<<${inputFunction}>>>,
+  fNA: ${firstNumbersArr[counter]},  oA: ${operatorsArr[counter]},  sNA: ${secondNubersArr[counter]},  eA: ${evaluationsArr[counter]},  cS: ${currentString},  uS: ${upperString}`;
+
+  // const text = `${counter}:  Input function: ${inputFunction},
+  // firstNumbersArr[${counter}]: ${firstNumbersArr[counter]},  operatorsArr[${counter}]: ${operatorsArr[counter]},  secondNumbersArr[${counter}]: ${secondNubersArr[counter]},  evaluationsArr[${counter}]: ${evaluationsArr[counter]},  currentString: ${currentString},  upperString: ${upperString}`;
+
+  const element = document.createElement("div");
+  element.classList.add("debug-content");
+  element.textContent = text;
+  element.style.color = color;
+  debugContainer.append(element);
 };
 
 btnAll.forEach((el) => {
@@ -67,7 +123,6 @@ btnAll.forEach((el) => {
     // console.log(e.target.dataset.btn);
     e.target.closest(".btn").style.color = "white";
     setTimeout(() => (e.target.closest(".btn").style.color = "black"), 300);
-
     const clicked = e.target.closest(".btn");
     btnClick(clicked.dataset.btn, clicked.dataset.type);
   });
@@ -79,7 +134,19 @@ btnAll.forEach((el) => {
     btnAll.forEach((el) => el.classList.remove("btn-mouseover"));
   });
 });
+
+debugClearBtn.addEventListener("click", function (e) {
+  debugContainer.textContent = "";
+});
+debugTestBtn.addEventListener("click", function () {
+  testFunction("testButton", "teal");
+});
+
 init();
+
+// firstNumbersArr[counter] = 5;
+// secondNubersArr[counter] = 2;
+// operatorsArr[counter] = "x";
 // if (e.target.closest(".btn").classList.contains("btn-num")) {
 //   const clicked = e.target.closest(".btn-num");
 //   btnNumClick(clicked.dataset.btn);
